4e108c7b8346f58b11ec81b1dcd5ad3aa181027a
andreasgrossmann <grossmann.andreas@gmail.com>
2016-08-30 17:31:07 +0100

    add and configure text fields

--- MemeMe/ViewController.swift
+++ MemeMe/ViewController.swift
@@ -8,21 +8,134 @@
 
 import UIKit
 
-class ViewController: UIViewController, UIImagePickerControllerDelegate, UINavigationControllerDelegate {
+class ViewController: UIViewController, UIImagePickerControllerDelegate, UINavigationControllerDelegate, UITextFieldDelegate {
 
+    @IBOutlet var topText: UITextField!
+    @IBOutlet var bottomText: UITextField!
     @IBOutlet var memeImage: UIImageView!
     @IBOutlet var cameraButton: UIBarButtonItem!
     
     override func viewDidLoad() {
         super.viewDidLoad()
         
+        
+        let centeringText = NSMutableParagraphStyle()
+        centeringText.alignment = .Center
+        
+        let memeTextAttributes = [
+            NSStrokeColorAttributeName: UIColor.blackColor(),
+            NSForegroundColorAttributeName: UIColor.whiteColor(),
+            NSFontAttributeName: UIFont(name: "HelveticaNeue-CondensedBlack", size: 40)!,
+            NSStrokeWidthAttributeName: -5.0,
+            NSParagraphStyleAttributeName: centeringText
+        ]
+        
+        topText.defaultTextAttributes = memeTextAttributes
+        bottomText.defaultTextAttributes = memeTextAttributes
+        
+
+        
+
+        
+        
+        topText.delegate = self
+        bottomText.delegate = self
+        
+
+        
     }
     
     override func viewWillAppear(animated: Bool) {
+        super.viewWillAppear(animated)
         // Disable camera button on devices that don't have a camera
         cameraButton.enabled = UIImagePickerController.isSourceTypeAvailable(UIImagePickerControllerSourceType.Camera)
+        
+        subscribeToKeyboardNotifications()
+    }
+    
+    override func viewWillDisappear(animated: Bool) {
+        super.viewWillDisappear(animated)
+        
+        unsubscribeFromKeyboardNotifications()
+    }
+    
+    
+    
+    
+    
+    
+    
+    
+    // Clear textfield when editing begins
+    func textFieldDidBeginEditing(textField: UITextField) {
+        textField.text = ""
     }
 
+    // When an empty string is entered, set textfield to default value
+    func textFieldDidEndEditing(textField: UITextField) {
+        topText.text == "" ? (topText.text = "TOP") : (topText.text = topText.text)
+        bottomText.text == "" ? (bottomText.text = "BOTTOM") : (bottomText.text = bottomText.text)
+    }
+    
+    
+    
+    
+    
+    
+    
+    // Make view slide up when keyboard shows and down again when keyboard hides
+    
+    func subscribeToKeyboardNotifications() {
+        NSNotificationCenter.defaultCenter().addObserver(self, selector: #selector(ViewController.keyboardWillShow(_:)), name: UIKeyboardWillShowNotification, object: nil)
+        NSNotificationCenter.defaultCenter().addObserver(self, selector: #selector(ViewController.keyboardWillHide(_:)), name: UIKeyboardWillHideNotification, object: nil)
+    }
+    
+    func unsubscribeFromKeyboardNotifications() {
+        NSNotificationCenter.defaultCenter().removeObserver(self, name:
+            UIKeyboardWillShowNotification, object: nil)
+        NSNotificationCenter.defaultCenter().removeObserver(self, name: UIKeyboardWillHideNotification, object: nil)
+    }
+    
+    func keyboardWillShow(notification: NSNotification) {
+        if bottomText.isFirstResponder() {
+            self.view.frame.origin.y =  getKeyboardHeight(notification) * -1
+        }
+    }
+    
+    func keyboardWillHide(notification: NSNotification) {
+        if bottomText.isFirstResponder() {
+            self.view.frame.origin.y = 0
+        }
+    }
+    
+    func getKeyboardHeight(notification: NSNotification) -> CGFloat {
+        let userInfo = notification.userInfo
+        let keyboardSize = userInfo![UIKeyboardFrameEndUserInfoKey] as! NSValue // of CGRect
+        return keyboardSize.CGRectValue().height
+    }
+    
+    
+    
+    
+    
+    
+
+
+    
+    
+    
+    // Keyboard hides when return key is pressed
+    
+    func textFieldShouldReturn(textField: UITextField) -> Bool {
+        self.view.endEditing(true)
+        return false
+    }
+    
+    
+    
+    
+    // Pick an existing image from the device
+    
     @IBAction func pickImage(sender: AnyObject) {
         
         let imagePicker = UIImagePickerController()
@@ -39,6 +152,7 @@ class ViewController: UIViewController, UIImagePickerControllerDelegate, UINavig
     }
     
     // Place selected image in the view
+    
     func imagePickerController(picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [String : AnyObject]) {
         if let image = info[UIImagePickerControllerOriginalImage] as? UIImage {
             memeImage.image = image
@@ -48,6 +162,7 @@ class ViewController: UIViewController, UIImagePickerControllerDelegate, UINavig
     
     
     
+    // Take a photo
     
     @IBAction func takePhoto(sender: AnyObject) {
         
